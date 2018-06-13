b8a9015bbbcfedb5ed347507d00618a22debdf99
MIDDLEEAST\ityer <itye.richter@microsoft.com>
2018-06-13 16:28:39 +0300

    Initial version
    Works as a web service.
    Todo: add cron job

---
+++ README.md
@@ -0,0 +1,7 @@
+# Kubernetes public IP Changer
+
+This application iterates via all servies with specific label, and recycles thier Service.
+
+This will cause to aquire a new public IP.
+
+This functionality is needed for some applications which don't want to be associated with a single public IP.
---
+++ index.js
@@ -0,0 +1,88 @@
+var express = require('express')
+const Client = require('kubernetes-client').Client
+const config = require('kubernetes-client').config;
+
+//setup an API client
+let client;
+try {
+    //assuming we are in the pod, try get the credentials from account service
+    client = new Client({ config: config.getInCluster(), version: '1.9' });
+} catch (e) {
+    //we must be debugging locally, than pickup credentials from kube config
+    client = new Client({ config: config.fromKubeconfig(), version: '1.9' });
+}
+
+//gather settings to operate
+const settings = {
+    LbRecycleLabel: process.env.LbRecycleLabel || "lbrecycle",
+    ServingPort: process.env.PortServiceServingPort || 4000
+}
+
+//start serving requests
+var app = express()
+app.listen(settings.ServingPort, function () {
+    console.log('Listening on port ' + settings.ServingPort);
+})
+
+
+app.get('/', function (req, res) {
+    res.send('Service is running');
+});
+
+app.get('/recycle', (request, response, next) => {
+
+    //make an API call for all services 
+    client.api.v1.services.get()
+        .then(function (services) {
+            //select only load balancers
+            let svcs = getServicesToRecycle(services);
+            svcs.forEach(svc => {
+                recycleService(svc);
+            });
+            response.send('Recycled ' + svcs.length + ' Services.');
+        })
+        .catch(next);
+});
+
+//Helper functions
+
+function getServicesToRecycle(services) {
+    //select only load balancers
+    let svcs = [];
+    services.body.items.forEach(service => {
+        if (settings.LbRecycleLabel != null && settings.LbRecycleLabel != "") {
+            for (var key in service.metadata.labels) {
+                if (key == "segasec" && service.metadata.labels[key] == settings.LbRecycleLabel) {
+                    svcs.push(service);
+                }
+            }
+        }
+    });
+    return svcs;
+}
+
+function recycleService(svc,callback){
+    console.log(svc);
+    client.api.v1.namespaces(svc.metadata.namespace).services(svc.metadata.name).delete()
+    .then(function(response){
+        delete svc.metadata.annotations;
+        delete svc.metadata.creationTimestamp;
+        delete svc.metadata.resourceVersion;
+        delete svc.metadata.selfLink;
+        delete svc.metadata.uid;
+        delete svc.__proto__;
+        delete svc.spec.clusterIP;
+        delete svc.spec.__proto__;
+        delete svc.__proto__;
+        delete svc.status;
+        
+        client.apis.v1.namespaces(svc.metadata.namespace).services.post({body: svc})
+        .then(function(svcresponse){
+            console.log(svcresponse);
+            //callback(svcresponse);
+        });
+    })
+    .catch(function(err){
+        console.log(err);
+    });
+}
