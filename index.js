ab915dc9d37e60c1622eeb1f48bed5bc97d403e1
MIDDLEEAST\ityer <itye.richter@microsoft.com>
2018-06-14 14:42:50 +0300

    added cron job in k8s

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ try {
 
 //gather settings to operate
 const settings = {
-    LbRecycleLabel: process.env.LbRecycleLabel || "lbrecycle",
     ServingPort: process.env.PortServiceServingPort || 4000
 }
 
@@ -30,59 +29,62 @@ app.get('/', function (req, res) {
 });
 
 app.get('/recycle', (request, response, next) => {
+    RecycleServices().then(function (length) {
+        response.send('Recycled ' + length + ' Services.');
+    })
+    .catch(next);
+});
 
+//Helper functions
+
+function RecycleServices() {
     //make an API call for all services 
-    client.api.v1.services.get()
+    return client.api.v1.services.get()
         .then(function (services) {
             //select only load balancers
             let svcs = getServicesToRecycle(services);
             svcs.forEach(svc => {
                 recycleService(svc);
             });
-            response.send('Recycled ' + svcs.length + ' Services.');
-        })
-        .catch(next);
-});
-
-//Helper functions
+            return svcs.length;
+        });
+}
 
 function getServicesToRecycle(services) {
     //select only load balancers
     let svcs = [];
     services.body.items.forEach(service => {
-        if (settings.LbRecycleLabel != null && settings.LbRecycleLabel != "") {
-            for (var key in service.metadata.labels) {
-                if (key == "segasec" && service.metadata.labels[key] == settings.LbRecycleLabel) {
-                    svcs.push(service);
-                }
+        for (var key in service.metadata.labels) {
+            if (key == "recycle" && service.metadata.labels[key] == "yes") {
+                svcs.push(service);
             }
         }
     });
     return svcs;
 }
 
-function recycleService(svc,callback){
+function recycleService(svc, callback) {
     console.log(svc);
     client.api.v1.namespaces(svc.metadata.namespace).services(svc.metadata.name).delete()
-    .then(function(response){
-        delete svc.metadata.annotations;
-        delete svc.metadata.creationTimestamp;
-        delete svc.metadata.resourceVersion;
-        delete svc.metadata.selfLink;
-        delete svc.metadata.uid;
-        delete svc.__proto__;
-        delete svc.spec.clusterIP;
-        delete svc.spec.__proto__;
-        delete svc.__proto__;
-        delete svc.status;
-        
-        client.apis.v1.namespaces(svc.metadata.namespace).services.post({body: svc})
-        .then(function(svcresponse){
-            console.log(svcresponse);
-            //callback(svcresponse);
+        .then(function (response) {
+            delete svc.metadata.annotations;
+            delete svc.metadata.creationTimestamp;
+            delete svc.metadata.resourceVersion;
+            delete svc.metadata.selfLink;
+            delete svc.metadata.uid;
+            delete svc.__proto__;
+            delete svc.spec.clusterIP;
+            delete svc.spec.__proto__;
+            delete svc.__proto__;
+            delete svc.status;
+
+            client.apis.v1.namespaces(svc.metadata.namespace).services.post({ body: svc })
+                .then(function (svcresponse) {
+                    console.log(svcresponse);
+                    //callback(svcresponse);
+                });
+        })
+        .catch(function (err) {
+            console.log(err);
         });
-    })
-    .catch(function(err){
-        console.log(err);
-    });
 }
